eded02e4b227268ae5a831d30b9f8d043c749f15
Nova Gh <nghimire.dev@gmail.com>
2022-11-21 11:32:14 -0800

    init tailwind & nextjs
    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <main className="min-h-screen flex ">
+        <h1 className="">test</h1>
+      </main>
+    </div>
+  );
+}
